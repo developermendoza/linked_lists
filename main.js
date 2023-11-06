0b38f22255d98c5d40fd2a0686b775eefcfdd8b3
Jose Mendoza <developermendoza@gmail.com>
2023-11-06 10:06:23 -0800

    add example 2

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
-class Node {
+// example 1
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   append(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//     } else {
+//       let current = this.head;
+//       while (current.next) {
+//         current = current.next;
+//       }
+//       current.next = newNode;
+//     }
+//   }
+//   print() {
+//     let current = this.head;
+//     while (current) {
+//       console.log(current.data);
+//       current = current.next;
+//     }
+//   }
+// }
+
+// const myList = new LinkedList();
+// myList.append(1);
+// myList.append(2);
+// myList.append(3);
+
+// myList.print();
+
+// example 2
+
+class ListNode {
   constructor(data) {
     this.data = data;
     this.next = null;
@@ -6,34 +49,15 @@ class Node {
 }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  append(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-  print() {
-    let current = this.head;
-    while (current) {
-      console.log(current.data);
-      current = current.next;
-    }
+  constructor(head = null) {
+    this.head = head;
   }
 }
 
-const myList = new LinkedList();
-myList.append(1);
-myList.append(2);
-myList.append(3);
+let node1 = new ListNode(2);
+let node2 = new ListNode(5);
+node1.next = node2;
 
-myList.print();
+let list = new LinkedList(node1);
+
+console.log(list.head.next.data);
