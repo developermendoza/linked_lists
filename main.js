e0f2253c1eb08c5c99b6965b479ae990beb7a0ca
Jose Mendoza <developermendoza@gmail.com>
2023-11-06 05:47:05 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const myList = new LinkedList();
+myList.append(1);
+myList.append(2);
+myList.append(3);
+
+myList.print();
